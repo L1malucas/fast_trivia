68083317f5c240dc5927916af11c4bc2f557a2ce
Lucas Lima <lucaslima.94@hotmail.com.br>
2023-08-20 21:36:36 -0300

    style: changes snack appereance

--- lib/src/core/ui/helpers/messages_helpers.dart
+++ lib/src/core/ui/helpers/messages_helpers.dart
@@ -5,15 +5,28 @@ import 'package:top_snackbar_flutter/top_snack_bar.dart';
 sealed class MessagesHelper {
   static void showError(String message, BuildContext context) {
     showTopSnackBar(
-        Overlay.of(context), CustomSnackBar.error(message: message));
+        snackBarPosition: SnackBarPosition.bottom,
+        displayDuration: const Duration(seconds: 1),
+        animationDuration: const Duration(seconds: 1),
+        Overlay.of(context),
+        CustomSnackBar.error(message: message));
   }
 
   static void showInfor(String message, BuildContext context) {
-    showTopSnackBar(Overlay.of(context), CustomSnackBar.info(message: message));
+    showTopSnackBar(
+        snackBarPosition: SnackBarPosition.bottom,
+        displayDuration: const Duration(seconds: 1),
+        animationDuration: const Duration(seconds: 1),
+        Overlay.of(context),
+        CustomSnackBar.info(message: message));
   }
 
   static void showSuccess(String message, BuildContext context) {
     showTopSnackBar(
-        Overlay.of(context), CustomSnackBar.success(message: message));
+        snackBarPosition: SnackBarPosition.bottom,
+        displayDuration: const Duration(seconds: 1),
+        animationDuration: const Duration(seconds: 1),
+        Overlay.of(context),
+        CustomSnackBar.success(message: message));
   }
 }
